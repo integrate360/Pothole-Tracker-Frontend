beac4203811569d9e531fc2b59d3dcd3620f48b2
unknown <integrate360@gmail.com>
2025-08-08 10:02:44 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import FinalReportPage from './pages/FinalReportPage';
 
 function App() {
   return (
     <div className="app">
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/mainpage" element={<MainPage />} />
+        <Route path="/finalreport" element={<FinalReportPage />} />
+      </Routes>
     </div>
   );
 }
